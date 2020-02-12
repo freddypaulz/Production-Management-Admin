7b76b248e28c1685c1d196bd111c1bcb1a9c2f2d
freddypaulz <52066593+freddypaulz@users.noreply.github.com>
2020-02-12 11:18:25 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//    MONGO_URI:
-//       'mongodb://localhost:27017/ProductionManagement?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
-// };
-module.exports = {
-   MONGO_URI:
-      'mongodb+srv://freddypaulz:mongo123@cluster0-6kohd.mongodb.net/ProductionManagement?retryWrites=true&w=majority'
-};
