95deb65cf774c3b6aa11355baebca26da2a0bf26
freddypaulz <freddypaulz@gmail.com>
2020-03-10 21:34:15 +0530

    Testing for deployment (trial)

---
+++ administrator/build/precache-manifest.9bfe530e04cacae322f828cdef6e6698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2551ff08e8d6ae20d75ffce51c10579f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65499f99006ea484d1b5",
+    "url": "/static/css/main.f98703cd.chunk.css"
+  },
+  {
+    "revision": "6ada6e1add614fa84c90",
+    "url": "/static/js/2.81c1ae2e.chunk.js"
+  },
+  {
+    "revision": "b440b940162570826c621f7853ba2305",
+    "url": "/static/js/2.81c1ae2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65499f99006ea484d1b5",
+    "url": "/static/js/main.a6a71496.chunk.js"
+  },
+  {
+    "revision": "db95a1b189a2fabf92ef",
+    "url": "/static/js/runtime-main.78c734b3.js"
+  }
+]);
